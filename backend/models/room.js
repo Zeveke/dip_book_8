6a72c24d77c2dd841910734b29cbb6c5f478fc77
Zeveke <143583225+Zeveke@users.noreply.github.com>
2024-05-12 20:56:33 +0400

    Update room.js

--- backend/models/room.js
+++ backend/models/room.js
@@ -2,14 +2,13 @@ const mongoose = require("mongoose");
 
 const roomSchema = new mongoose.Schema(
   {
-    name: {type: String,required: true},
-    maxcount: {type: Number,required: true},
-    phoneNumber: {type: Number,required: true},
-    rentperday: {type: Number,required: true},
-    image: [],
-    currentbookings: [],
-    type: {type: String,required: true},
-    description: {type: String,required: true},
+    name: { type: String, required: true },
+    maxcount: { type: Number, required: true },
+    phoneNumber: { type: Number, required: true },
+    rentperday: { type: Number, required: true },
+    image: { type: [String], required: true },
+    currentbookings: [{ type: mongoose.Schema.Types.ObjectId }],
+    description: { type: String, required: true },
   },
   {
     timestamps: true,
@@ -19,5 +18,7 @@ const roomSchema = new mongoose.Schema(
 const Room = mongoose.model("rooms", roomSchema);
 
 module.exports = Room;
+
+
 
 
