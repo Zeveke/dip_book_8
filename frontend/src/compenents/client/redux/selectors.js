4ead7f95de59e83d20ee28cd481d1581016c72e2
Zeveke <143583225+Zeveke@users.noreply.github.com>
2024-05-12 20:38:16 +0400

    Add files via upload

---
+++ frontend/src/compenents/client/redux/selectors.js
@@ -0,0 +1,4 @@
+export const roomsSelector = (state) => state.roomsState.rooms;
+export const profileSelector = (state) => state.prifileState.rooms;
+export const loadingSelector = (state) => state.loadingSelector.loading;
+export const reservationsSelector = (state) => state.reservatuionsState.rooms;
