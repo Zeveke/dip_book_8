524a3fd07eb2b95bf3afe0c04753890b2cbcc042
Zeveke <143583225+Zeveke@users.noreply.github.com>
2024-05-12 21:05:02 +0400

    Update selectors.js

--- frontend/src/compenents/client/redux/selectors.js
+++ frontend/src/compenents/client/redux/selectors.js
@@ -1,4 +1,4 @@
 export const roomsSelector = (state) => state.roomsState.rooms;
-export const profileSelector = (state) => state.prifileState.rooms;
+export const profileSelector = (state) => state.profileState.rooms;
 export const loadingSelector = (state) => state.loadingSelector.loading;
 export const reservationsSelector = (state) => state.reservatuionsState.rooms;
