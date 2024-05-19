3d85fe4a22ce37fa04ec2506aecdb2fedba2d481
Zeveke <143583225+Zeveke@users.noreply.github.com>
2024-05-19 12:28:02 +0400

    Delete frontend/src/compenents/client/redux directory

--- frontend/src/compenents/client/redux/selectors.js
+++
@@ -1,4 +0,0 @@
-export const roomsSelector = (state) => state.roomsState.rooms;
-export const profileSelector = (state) => state.profileState.rooms;
-export const loadingSelector = (state) => state.loadingSelector.loading;
-export const reservationsSelector = (state) => state.reservatuionsState.rooms;
