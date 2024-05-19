0efdf9221825dfad044cac215eef2ca4eed64b76
Zeveke <143583225+Zeveke@users.noreply.github.com>
2024-05-19 12:30:20 +0400

    Add files via upload

--- frontend/src/sevices/http-common.js
+++ frontend/src/sevices/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "http://217.25.90.141:3005",
+  baseURL: "process.env.REACT_APP_API_URL",
   headers: { "Content-Type": "application/json" },
 });
 
