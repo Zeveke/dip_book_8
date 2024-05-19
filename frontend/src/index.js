5664dc30c7d844a5b58ea185b2abce0d7691e521
Zeveke <143583225+Zeveke@users.noreply.github.com>
2024-05-19 11:46:24 +0400

    Add files via upload

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./compenents/redux/store";
 
 import App from "./App";
-import store from "./compenents/client/redux/store";
 
-const root = createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Provider store={store}>
